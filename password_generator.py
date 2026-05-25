fe1ba9912e6206308185279cd663c30095098dac
Insidious Echo <thestkr007@gmail.com>
2026-05-25 15:27:22 +0000

    shipped password generator

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,11 @@
+import random
+import string
 
+def make_password(length):
+    pool = string.ascii_letters + string.digits + string.punctuation
+    result = "".join(random.choice(pool) for i in range(length))
+    (print("🔒 New Password:", result))
+
+(make_password(16))
+
+        

ea7e343131d165516d8defb51200735d4eb6c14c
Insidious Echo <thestkr007@gmail.com>
2026-05-26 19:23:23 +0000

    Day 2: Added interactive terminal input and input validation defenses

--- password_generator.py
+++ password_generator.py
@@ -6,6 +6,24 @@ def make_password(length):
     result = "".join(random.choice(pool) for i in range(length))
     (print("🔒 New Password:", result))
 
-(make_password(16))
+while True:
+#1.ask user for input and save it as a variable called 'user_input'
+    user_input = input ("Enter desired password length (minimum 8):")
+    #Gate 1: Is it a number?
+    if user_input.isdigit():
+        length = int(user_input)
+
+        #Gate 2: Is it long enough? (Notice this is indented inside Gate 1)
+        if length >=8:
+            #1 call your function here: make_password (le#2 Put your break here to exit!
+            make_password(length)
+            break
+    else:
+        print ("❌️ Too Weak! Passwords must be 8 or more characters")
+else:
+        print ("❌️ Error: Those are not valid numbers. Use digits only")   
+    #Converts the text "12" into maths number 12
+
+
 
         
